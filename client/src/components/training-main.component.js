6770b18c2282d07c0a4da6c8fa0416b133965f37
Kelvin Chow <kelvinchow223@gmail.com>
2020-12-12 02:24:38 -0500

    add keyboard event handler for training

--- client/src/components/training-main.component.js
+++ client/src/components/training-main.component.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
+
 
  export default class TrainingMain extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            letter: ''
+        }
+    }
+
+    startLogging(key) {
+        //Display next letter
+        this.setState({
+            letter: key
+        });
+        this.nameInput.focus();
+        //Turn on green light, start countdown timer, Focus on hidden form element
+    }
 
     render() {
-        return (
+        return (     
             <div>
                 <div>
-                <p>MAIN TRAINING STUFF HERE.</p>
+                <p>Hit some keys.</p>
+                <KeyboardEventHandler 
+                    handleKeys= {['all']} 
+                    onKeyEvent={(key) => this.startLogging(key) }/>
                 </div>
+                <h1>{this.state.letter}</h1>
+                <div style={{opacity: 100}}>
+                    <input 
+                    type='text'className='hiddenInput' 
+                    ref={(input) => { this.nameInput = input; }}  />
+                </div>
+                
             </div>
         )
     }
