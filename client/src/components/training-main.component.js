89285b228bf96df62f634cf868fb6be93db4d135
Kelvin Chow <kelvinchow223@gmail.com>
2020-12-12 15:16:19 -0500

    update training page escape key

--- client/src/components/training-main.component.js
+++ client/src/components/training-main.component.js
@@ -6,33 +6,55 @@ import KeyboardEventHandler from 'react-keyboard-event-handler';
     constructor (props) {
         super(props);
         this.state = {
-            letter: ''
+            letter: '',
+            logdata: ''
         }
     }
 
-    startLogging(key) {
+    startLog(key) {
         //Display next letter
         this.setState({
             letter: key
         });
         this.nameInput.focus();
-        //Turn on green light, start countdown timer, Focus on hidden form element
+        //Turn on green light, start countdown timer,
+    }
+
+    logging(e) {
+        this.setState({
+            logdata: e.target.value
+        });
+    }
+
+    sendLog(key) {
+        console.log(this.state.logdata);
+        this.nameInput.blur();
+        //TODO send http request to log data
+        this.setState({
+            logdata: ''
+        })
     }
 
     render() {
         return (     
             <div>
                 <div>
-                <p>Hit some keys.</p>
+                <p>Hit escape key.</p>
                 <KeyboardEventHandler 
-                    handleKeys= {['all']} 
-                    onKeyEvent={(key) => this.startLogging(key) }/>
+                    handleKeys= {['esc']} 
+                    onKeyEvent={(key) => this.startLog(key) }/>
                 </div>
                 <h1>{this.state.letter}</h1>
                 <div style={{opacity: 100}}>
-                    <input 
-                    type='text'className='hiddenInput' 
-                    ref={(input) => { this.nameInput = input; }}  />
+                <KeyboardEventHandler 
+                    handleKeys= {['esc']}
+                    onKeyEvent={(key) => this.sendLog(key)} >
+                        <input 
+                        type='text' value = {this.state.logdata}
+                        onChange = {this.logging.bind(this)}
+                        ref={(input) => { this.nameInput = input; }}  />
+                </KeyboardEventHandler>
+                    
                 </div>
                 
             </div>
