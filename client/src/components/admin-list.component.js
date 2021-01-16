8e1a2eed5ef52299a7cc3c8ae523f0e862702534
Kelvin Chow <kelvinchow223@gmail.com>
2021-01-15 19:06:08 -0500

    update admin consle formatting, add data export button

--- client/src/components/admin-list.component.js
+++ client/src/components/admin-list.component.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
+import {CSVLink} from 'react-csv';
 
  export default class AdminList extends Component {
     constructor(props) {
@@ -10,11 +11,35 @@ import Button from 'react-bootstrap/Button';
         this.state = {
             profileData: [],
             trainingData: [],
-            showTable: false
+            showTable: false,
+            currentStatus: '',
+            currentUser: '',
         };
     }
 
-    renderUserRequestList() {
+
+    renderStatusList() {
+        let uniqueStatus = this.state.profileData.map(user => user.status)
+            .filter((value, index, self) => self.indexOf(value) === index);
+        return uniqueStatus.map((status) => {
+            return (
+                <option value ={status}>{status}</option>
+            )
+        })
+    }
+
+    renderUserRequestList(selectStatus) {
+            const newUserList = this.state.profileData.filter(item => item.status === selectStatus);
+                return newUserList.map((user) => {
+                    const {username} = user;
+                    return (
+                        <option value={username}>{username}</option>
+                    )
+                }
+            )
+    }
+
+    renderAllUserList() {
         return this.state.profileData.map((user) => {
             const {username} = user;
             return(
@@ -23,47 +48,54 @@ import Button from 'react-bootstrap/Button';
         })
     }
 
-    renderUserList() {
-        return this.state.profileData.map((user) => {
-            const {username} = user;
-            return(
-                <option value={username}>{username}</option>
-            )
-        })
-    }
-
-    downloadData() {
-        alert('TODO: download CSV data');
-    }
-
-    renderLogTable(username) {
-        console.log(username);
-        return (
-            <React.Fragment>
-                {<div>
-                    <p>User: {username} </p><Button onClick={this.downloadData.bind(this)}>Download CSV</Button>
-                </div>}
-                <Table className ='col-9'>
-                    <thead>
-                        <tr>
-                            <th>Letter</th>
-                            <th>Time</th>
-                            <th>Data</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.trainingData.map(training => {
-                            return(
+    renderLogTable() {
+        let username = this.state.currentUser;
+        if (username !== '') {
+            console.log(this.state.trainingData);
+            console.log(this.state.trainingData.length !== 0);
+            if (this.state.trainingData.length !== 0) {
+                return (
+                    <React.Fragment>
+                        {<div className = 'row mb-3'>
+                            <h4 className = 'col-9'>
+                                User: {username}
+                            </h4>
+                            <Button variant = 'outline-primary' className = 'col-3'>
+                                <CSVLink
+                                    data = {this.state.trainingData}
+                                    filename = {username + '-data.csv'}
+                                >
+                                    Download User Data
+                                </CSVLink>
+                            </Button>
+                        </div>}
+                        <Table>
+                            <thead>
                                 <tr>
-                                    <td>{training.letter}</td>
-                                    <td>{training.datetime}</td>
-                                    <td className='truncate-table-data'>{training.data}</td>
+                                    <th>Letter</th>
+                                    <th>Time</th>
+                                    <th className='truncate-table-data'>Data</th>
                                 </tr>
-                        )})}
-                    </tbody>
-                </Table>
-            </React.Fragment>
-        )
+                            </thead>
+                            <tbody>
+                                {this.state.trainingData.map(training => {
+                                    return(
+                                        <tr>
+                                            <td>{training.letter}</td>
+                                            <td>{training.datetime}</td>
+                                            <td className='truncate-table-data'>{training.data}</td>
+                                        </tr>
+                                )})}
+                            </tbody>
+                        </Table>
+                    </React.Fragment>
+                )
+            } else {
+                return (
+                    <h4> {username} has no training data.  Please choose another user.</h4>
+                )
+            }
+        }
     }
 
     getAllUsers() {
@@ -81,52 +113,57 @@ import Button from 'react-bootstrap/Button';
         axios.get(window.location.href+'/getTrainingData/'+username)
             .then(res => {
                 if (res.data) {
-                    console.log(res.data);   
                     this.setState({
                         trainingData : res.data,
+                        currentUser: username,
                         showTable: true
                     }); 
                 }
-                this.renderLogTable(username);
+                this.renderLogTable();
             })
     }
 
     onUserSelect(e) {
         this.getTrainingData(e.target.value);
     }
-
-    sayhello(user) {
-        alert('hello '+ user);
+    
+    onRequestFilter(e) {
+        this.setState({currentStatus: e.target.value})
+        this.renderUserRequestList(e.target.value);
     }
 
     componentDidMount() {
         this.getAllUsers();
+        //this.renderUserRequestList(this.state.currentStatus);
     }
 
     render() {
         return (
             <div>
-                <p>Things to do: get users: 1) Training Requests 2) Get all users 3)Get table of all user data from selected user</p>
                 <h3>Admin Console</h3>
-                <h4>User Training Requests</h4>
-                <h4>User Data Explorer</h4>
-                <p>Click on a specific user to view their training data.  For now, testuser has training data</p>
+                <p>Click on a specific user to view their training data.</p>
                 <div className='row'>
-                    <Form className='col-2'>
-                        <Form.Group controlId='userSelect'>
-                            <Form.Label>Select User:</Form.Label>
-                            <Form.Control as='select' multiple onChange={this.onUserSelect.bind(this)}>
-                                {this.renderUserList()}
+                    <Form className='col-3'>
+                        <Form.Group controlId='userRequestSelect'>
+                            <Form.Label>
+                                <strong>Filter Users by Status:</strong>
+                                <Form.Control as = 'select' onChange = {this.onRequestFilter.bind(this)}>
+                                    <option hidden disabled selected value>Please Select Status...</option>
+                                    {this.renderStatusList()}
+                                </Form.Control>                            
+                            </Form.Label>
+                            <Form.Control as = 'select' multiple onChange={this.onUserSelect.bind(this)}>
+                                {this.renderUserRequestList(this.state.currentStatus)}
                             </Form.Control>
                         </Form.Group>
-                        <Form.Group controlId='userRequestSelect'>
-                            <Form.Label>Users with Request:</Form.Label>
+                        <Form.Group controlId='userAllSelect'>
+                            <Form.Label><strong>Select from All Users:</strong></Form.Label>
                             <Form.Control as='select' multiple onChange={this.onUserSelect.bind(this)}>
-                                {this.renderUserRequestList()}
+                                {this.renderAllUserList()}
                             </Form.Control>
                         </Form.Group>
                     </Form>
-                    <div className='col-9 offset-1'>
+                    <div className='col-9'>
                         {this.state.showTable && this.renderLogTable()}
                     </div>
                 </div>
