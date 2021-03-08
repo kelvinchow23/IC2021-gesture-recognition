aba5035ac37c7d785043cbd8c14e71c215aea66b
Kelvin Chow <kelvinchow223@gmail.com>
2021-03-08 00:02:02 -0500

    updated daschboard process images + tooltip

--- client/src/components/dashboard-list.component.js
+++ client/src/components/dashboard-list.component.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import Image from 'react-bootstrap/image';
+import Card from 'react-bootstrap/card';
 import flourishLogo from './images/flourish-logo-circle-large.png';
 import gestureWrist from './images/gesture-wrist.jpg';
 import gestureLeg from './images/gesture-leg.jpg';
 import tflogo from './images/tensorflow.png';
-
-
+import personOutline from './images/person-outline-2.png';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 
  class DashboardList extends Component {
     render() {
@@ -38,18 +40,92 @@ import tflogo from './images/tensorflow.png';
                 </div>     
                 <h3>The Process</h3>
                 <div className='row mb-5'>
-                    <div className='col-1'>
-                        <div className='circle'>1</div> 
+                    <div className='col-4'>
+                        <div className='circle'>1
+                            <span className='faux-white-text'>__</span>
+                            <span className='colourful'>CONFIGURE</span>
+                            <span className='faux-white-text'>____</span>
+                            <span className='colourful'>➤</span>
+                        </div>
+                            <Card className='person-container'>
+                                <Card.Img src={personOutline} alt='person outline' />
+                                <Card.ImgOverlay>
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  HEAD: limited functionality very visible
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-head'>●</div>
+                                </OverlayTrigger>
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  TRUNK: Big wrap, medium functionality
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-trunk'>●</div>
+                                </OverlayTrigger>
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  SHOULDER: medium functionality
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-shoulder'>●</div>
+                                </OverlayTrigger>
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  ARM: optimal choice, low visiblity and high functionality
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-arm'>●</div>
+                                </OverlayTrigger>
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  HAND: optimal choice, high functionality
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-hand'>●</div>
+                                </OverlayTrigger> 
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  HIP: low functionality
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-hip'>●</div>
+                                </OverlayTrigger>  
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  LEG: good choice, medium functionality
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-leg'>●</div>
+                                </OverlayTrigger>  
+                                <OverlayTrigger placement='right' overlay={
+                                                                  <Tooltip id='tooltip-head'>
+                                                                  FOOT: good choice
+                                                                  </Tooltip>}
+                                >
+                                    <div className='dot dot-foot'>●</div>
+                                </OverlayTrigger>                                                                
+                                    <div className='dot-computer'></div>
+                                </Card.ImgOverlay>
+                            </Card>                        
                     </div>
-                    <div className='col-2 nice-text'>CONFIGURE</div>
-                    <div className='col-1 offset-1'>
-                        <div className='circle'>2</div> 
+                    <div className='col-3 offset-1'>
+                        <div className='circle'>2
+                            <span className='faux-white-text'>__</span>
+                            <span className='colourful'>TRAIN</span>
+                            <span className='faux-white-text'>____</span>
+                            <span className='colourful'>➤</span>
+                        </div>
                     </div>
-                    <div className='col-2 nice-text'>TRAIN</div>
-                    <div className='col-1 offset-1'>
-                        <div className='circle'>3</div> 
+                    <div className='col-3 offset-1'>
+                        <div className='circle'>3
+                            <span className='faux-white-text'>__</span>
+                            <span className='colourful'>FLOURISH</span>
+                            <span className='faux-white-text'>___</span>
+                            <span className='faux-white-text'>➤</span>
+                        </div>
                     </div>
-                    <div className='col-2 nice-text'>FLOURISH</div>
                 </div>
 
                 <h3>The Technology</h3>
@@ -63,7 +139,7 @@ import tflogo from './images/tensorflow.png';
                 <p>Welcome to flourish portal.  This application allows unique individuals to 
                 control electronic devices with custom user-defined gestures.  To use this application,
                 you will need two <a href = 'https://www.digikey.com/en/products/detail/m5stack-technology-co-ltd/K016-D/10492136'>M5StickC units</a>. 
-                Further hardware configuration details and firmware upload instructions can be found <a href=''>here</a>. </p>
+               </p>
                 <p>Please login to begin.</p>
             </div>
         )
